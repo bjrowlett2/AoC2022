3ebf62b943145eb55d2284bb44d4d858dbd51c7b
Brian Rowlett <bjrowlett2@gmail.com>
2022-12-10 09:45:10 -0600

    Refactored solution for Day 10

--- cmd/day_10/main.go
+++ cmd/day_10/main.go
@@ -54,12 +54,11 @@ func Load() (*Problem, error) {
 	return &problem, nil
 }
 
-func (problem *Problem) SolvePart1() error {
-	var err error
+type CycleFunc func(x, cycle int)
 
+func Run(problem *Problem, fn CycleFunc) error {
 	x := 1
 	cycle := 0
-	strength := 0
 
 	for _, inst := range problem.Instructions {
 		temp := 0
@@ -68,7 +67,7 @@ func (problem *Problem) SolvePart1() error {
 			remaining = 1
 		} else {
 			var value int
-			if _, err = fmt.Sscanf(inst, "addx %d", &value); err != nil {
+			if _, err := fmt.Sscanf(inst, "addx %d", &value); err != nil {
 				return err
 			}
 
@@ -78,15 +77,29 @@ func (problem *Problem) SolvePart1() error {
 
 		for remaining > 0 {
 			cycle += 1
-			if (cycle-20)%40 == 0 {
-				strength += cycle * x
-			}
+			fn(x, cycle)
 
 			remaining -= 1
 			if remaining == 0 {
 				x += temp
 			}
 		}
+	}
+
+	return nil
+}
+
+func (problem *Problem) SolvePart1() error {
+	strength := 0
+
+	err := Run(problem, func(x, cycle int) {
+		if (cycle-20)%40 == 0 {
+			strength += cycle * x
+		}
+	})
+
+	if err != nil {
+		return err
 	}
 
 	fmt.Printf("Part 1: %d\n", strength)
@@ -94,46 +107,21 @@ func (problem *Problem) SolvePart1() error {
 }
 
 func (problem *Problem) SolvePart2() error {
-	var err error
-
-	x := 1
-	cycle := 0
-
 	screen := make([][]bool, 6)
 	for i := 0; i < len(screen); i++ {
 		screen[i] = make([]bool, 40)
 	}
 
-	for _, inst := range problem.Instructions {
-		temp := 0
-		remaining := 0
-		if inst == "noop" {
-			remaining = 1
-		} else {
-			var value int
-			if _, err = fmt.Sscanf(inst, "addx %d", &value); err != nil {
-				return err
-			}
-
-			temp = value
-			remaining = 2
+	err := Run(problem, func(x, cycle int) {
+		row := (cycle - 1) / 40
+		column := (cycle - 1) % 40
+		if aoc.Between(x-1, column, x+1) {
+			screen[row][column] = true
 		}
+	})
 
-		for remaining > 0 {
-			cycle += 1
-			idx := cycle - 1
-
-			row := idx / 40
-			column := idx % 40
-			if aoc.Between(x-1, column, x+1) {
-				screen[row][column] = true
-			}
-
-			remaining -= 1
-			if remaining == 0 {
-				x += temp
-			}
-		}
+	if err != nil {
+		return err
 	}
 
 	for row := 0; row < len(screen); row++ {
