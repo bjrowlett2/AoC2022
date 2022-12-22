68ee7ee1c5a17686e8b7090e119e8c3ad6c2c062
Brian Rowlett <bjrowlett2@gmail.com>
2022-12-21 18:51:33 -0600

    Added solution for Day 21

---
+++ cmd/day_21/main.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	var err error
+
+	var problem1 *Problem
+	if problem1, err = Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = problem1.SolvePart1(); err != nil {
+		log.Fatal(err)
+	}
+
+	var problem2 *Problem
+	if problem2, err = Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = problem2.SolvePart2(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type MonkeyType int
+
+const (
+	MonkeyTypeNumber MonkeyType = iota
+	MonkeyTypeFormula
+)
+
+type Expression struct {
+	Left     string
+	Right    string
+	Operator string
+}
+
+type Monkey struct {
+	Name    string
+	Type    MonkeyType
+	Number  int64
+	Formula Expression
+}
+
+type Problem struct {
+	Monkeys map[string]Monkey
+}
+
+func Load() (*Problem, error) {
+	var err error
+
+	var file *os.File
+	name := "day_21.txt"
+	if file, err = os.Open(name); err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	problem := Problem{
+		Monkeys: make(map[string]Monkey),
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+
+		monkey := Monkey{
+			Name: parts[0],
+		}
+
+		if len(parts[1]) < 9 { // @Hack
+			monkey.Type = MonkeyTypeNumber
+			fmt.Sscanf(parts[1], " %d", &monkey.Number)
+		} else {
+			monkey.Type = MonkeyTypeFormula
+
+			formula := &monkey.Formula
+			fmt.Sscanf(parts[1], " %s %s %s", &formula.Left, &formula.Operator, &formula.Right)
+		}
+
+		problem.Monkeys[monkey.Name] = monkey
+	}
+
+	return &problem, nil
+}
+
+func Yell(problem *Problem, name string) int64 {
+	monkey := problem.Monkeys[name]
+
+	var number int64 = 0
+	switch monkey.Type {
+	case MonkeyTypeNumber:
+		return monkey.Number
+
+	case MonkeyTypeFormula:
+		formula := monkey.Formula
+		left := Yell(problem, formula.Left)
+		right := Yell(problem, formula.Right)
+
+		switch formula.Operator {
+		case "+":
+			number = left + right
+		case "-":
+			number = left - right
+		case "*":
+			number = left * right
+		case "/":
+			number = left / right
+		}
+	}
+
+	return number
+}
+
+func (problem *Problem) SolvePart1() error {
+	root := Yell(problem, "root")
+	fmt.Printf("Part 1: %d\n", root)
+	return nil
+}
+
+type Side int
+
+const (
+	SideUnknown Side = iota
+	SideLeft
+	SideRight
+)
+
+func FindHuman(problem *Problem, name string) Side {
+	monkey := problem.Monkeys[name]
+
+	if monkey.Type == MonkeyTypeFormula {
+		formula := monkey.Formula
+		if formula.Left == "humn" {
+			return SideLeft
+		} else if formula.Right == "humn" {
+			return SideRight
+		}
+
+		if FindHuman(problem, formula.Left) != SideUnknown {
+			return SideLeft
+		} else if FindHuman(problem, formula.Right) != SideUnknown {
+			return SideRight
+		}
+	}
+
+	return SideUnknown
+}
+
+func (problem *Problem) SolvePart2() error {
+	// First, you got the wrong job for the monkey named root;
+	// specifically, you got the wrong math operation. The correct
+	// operation for monkey root should be =.
+	root := problem.Monkeys["root"]
+	problem.Monkeys["root"] = Monkey{
+		Type:   root.Type,
+		Number: root.Number,
+		Formula: Expression{
+			Left:     root.Formula.Left,
+			Right:    root.Formula.Right,
+			Operator: "=",
+		},
+	}
+
+	name := "root"
+	var answer int64 = 0
+	for name != "humn" {
+		monkey := problem.Monkeys[name]
+
+		var result int64
+		formula := monkey.Formula
+		switch FindHuman(problem, name) {
+		case SideLeft:
+			name = formula.Left
+			result = Yell(problem, formula.Right)
+
+			switch formula.Operator {
+			case "+":
+				answer = answer - result
+			case "-":
+				answer = answer + result
+			case "*":
+				answer = answer / result
+			case "/":
+				answer = answer * result
+			case "=":
+				answer = result
+			}
+		case SideRight:
+			name = formula.Right
+			result = Yell(problem, formula.Left)
+
+			switch formula.Operator {
+			case "+":
+				answer = answer - result
+			case "-":
+				answer = result - answer
+			case "*":
+				answer = answer / result
+			case "/":
+				answer = result / answer
+			case "=":
+				answer = result
+			}
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", answer)
+	return nil
+}
